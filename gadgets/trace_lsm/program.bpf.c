80feac6ba5e805c51aa7d0ca38d217e792f5d3fd
Tianyi Liu <i.pear@outlook.com>
2024-04-22 18:39:57 +0800

    test

--- gadgets/trace_lsm/program.bpf.c
+++ gadgets/trace_lsm/program.bpf.c
@@ -7,6 +7,7 @@
 
 #include <gadget/buffer.h>
 #include <gadget/macros.h>
+#include <gadget/mntns_filter.h>
 
 enum memop {
 	MALLOC,
@@ -23,15 +24,22 @@ GADGET_TRACER_MAP(events, 1024 * 256);
 
 GADGET_TRACER(malloc, events, event);
 
-SEC("lsm/bpf")
-int trace_bpf(int cmd, union bpf_attr *uattr, unsigned int size)
+
+SEC("lsm/mmap_file")
+int trace_bpf(struct file *file, unsigned long reqprot, unsigned long prot, unsigned long flags)
 {
+	__u64 mntns_id = gadget_get_mntns_id();
+	const char fmt_str[] = "%lu\n";
+	bpf_trace_printk(fmt_str, sizeof(fmt_str), mntns_id);
 	struct event e;
 	e.pid = 1;
-	e.operation = MALLOC;
+	e.operation = mntns_id % 2;
 	e.addr = 2;
 
 	bpf_ringbuf_output(&events, &e, sizeof(e), 0);
+
+	if (gadget_should_discard_mntns_id(mntns_id))
+		return 0;
 	return 0;
 }
 
