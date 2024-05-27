711c7d6de9129714620dcfecb7c71edffe2bcbbb
Tianyi Liu <i.pear@outlook.com>
2024-05-27 15:57:36 +0800

    1

--- pkg/ustackunwinder/bpf/ustack_unwinder.bpf.c
+++ pkg/ustackunwinder/bpf/ustack_unwinder.bpf.c
@@ -50,3 +50,47 @@ struct {
 	__uint(max_entries, USTACK_MAX_UNWIND_INFO_ENTRIES);
 } __ustack_unwind_infos SEC(".maps");
 
+static ErrorCode unwind_one_frame(u64 pid, u32 frame_idx, UnwindState *state, bool* stop) {
+  *stop = false;
+
+  u32 unwindInfo = 0;
+  u64 rt_regs[18];
+  int addrDiff = 0;
+  u64 cfa = 0;
+
+  // The relevant executable is compiled with frame pointer omission, so
+  // stack deltas need to be retrieved from the relevant map.
+  ErrorCode error = get_stack_delta(state->text_section_id, state->text_section_offset,
+                                    &addrDiff, &unwindInfo);
+  if (error) {
+    return error;
+  }
+
+
+    UnwindInfo *info = bpf_map_lookup_elem(&unwind_info_array, &unwindInfo);
+    if (!info) {
+      return -1;
+    }
+
+    s32 param = info->param;
+
+    // Resolve the frame's CFA (previous PC is fixed to CFA) address, and
+    // the previous FP address if any.
+    cfa = unwind_register_address(state, 0, info->opcode, param);
+    u64 fpa = unwind_register_address(state, cfa, info->fpOpcode, info->fpParam);
+
+    if (fpa) {
+      bpf_probe_read(&state->fp, sizeof(state->fp), (void*)fpa);
+    } else if (info->opcode == UNWIND_OPCODE_BASE_FP) {
+      // FP used for recovery, but no new FP value received, clear FP
+      state->fp = 0;
+    }
+  
+
+  if (!cfa || bpf_probe_read(&state->pc, sizeof(state->pc), (void*)(cfa - 8))) {
+  err_native_pc_read:
+    increment_metric(metricID_UnwindNativeErrPCRead);
+    return -1;
+  }
+  state->sp = cfa;
+}
